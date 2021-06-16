ded2d2a474a260367d9348e3682db4298352a543
Lexxkit <lexxkit@gmail.com>
2021-06-16 13:35:00 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+//const { request } = require('express');
 dotenv.config();
 const app = new express();
 
-function getNLUInstance() {
-    let api_key = process.API_KEY;
-    let api_url = process.API_URL;
+const getNLUInstance = ()=> {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
     
     const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
     const { IamAuthenticator } = require('ibm-watson/auth');
@@ -18,7 +19,7 @@ function getNLUInstance() {
         serviceUrl: api_url,
     });
     return naturalLanguageUnderstanding;
-};
+}
 
 app.use(express.static('client'))
 
@@ -30,20 +31,99 @@ app.get("/",(req,res)=>{
   });
 
 app.get("/url/emotion", (req,res) => {
+    const analyzeParams = {
+        'url':req.query.url,
+        'features': {
+            'entities': {
+                'emotion': true,
+                'limit': 1
+            }
+        }
+    }
 
-    return res.send({"happy":"90","sad":"10"});
+    const naturalLanguageUnderstanding = getNLUInstance();
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            console.log(analysisResults);
+            console.log(JSON.stringify(analysisResults.result.entities[0].emotion, null, 2));
+        
+        return res.send(analysisResults.result.entities[0].emotion,null,2);
+        })
+        .catch(err => {
+            return res.send("Could not do desired operation "+err);
+        });
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    const analyzeParams = {
+        'url':req.query.url,
+        'features': {
+            'entities': {
+                'sentiment': true,
+                'limit': 1
+            }
+        }
+    }
+
+    const naturalLanguageUnderstanding = getNLUInstance();
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            console.log(analysisResults);
+            console.log(JSON.stringify(analysisResults.result.entities[0].sentiment, null, 2));
+        
+        return res.send(analysisResults.result.entities[0].sentiment.label,null,2);
+        })
+        .catch(err => {
+            return res.send("Could not do desired operation "+err);
+        });
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    const analyzeParams = {
+        'text':req.query.text,
+        'features': {
+            'entities': {
+                'emotion': true,
+                'limit': 1
+            }
+        }
+    }
+
+    const naturalLanguageUnderstanding = getNLUInstance();
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            console.log(analysisResults);
+            console.log(JSON.stringify(analysisResults.result.entities[0].emotion, null, 2));
+        
+        return res.send(analysisResults.result.entities[0].emotion,null,2);
+        })
+        .catch(err => {
+            return res.send("Could not do desired operation "+err);
+        });
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    const analyzeParams = {
+        'text':req.query.text,
+        'features': {
+            'entities': {
+                'sentiment': true,
+                'limit': 1
+            }
+        }
+    }
+
+    const naturalLanguageUnderstanding = getNLUInstance();
+    naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+            console.log(analysisResults);
+            console.log(JSON.stringify(analysisResults.result.entities[0].sentiment, null, 2));
+        
+        return res.send(analysisResults.result.entities[0].sentiment.label,null,2);
+        })
+        .catch(err => {
+            return res.send("Could not do desired operation "+err);
+        });
 });
 
 let server = app.listen(8080, () => {
